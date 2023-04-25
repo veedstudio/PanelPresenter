4910aae5ddc8844540929bc9ef9aaf8986352fc6
Pim <pim@pixelrock.nl>
2023-04-25 15:39:57 +0200

    Use forked repo of ConstraintBuilder

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["PanelPresenter"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/PimCoumans/ConstraintBuilder", "1.0.4"..<"2.0.0")
+		.package(url: "https://github.com/veedstudio/ConstraintBuilder", "1.0.4"..<"2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
